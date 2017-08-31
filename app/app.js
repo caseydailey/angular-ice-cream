0d2344cea10eddd0560f9f226944f4f6cbe0dd9d
Casey Dailey <casey@feasttogether.org>
2017-08-31 09:17:39 -0500

    all set

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // declaring the app as module. we can now put this on the body with 'ng-app'
-var app = angular.module("IceCreamApp", ['ngRoute']);
+var app = angular.module("IceCreamApp", ['ngRoute', ]);
 
 app.config(function($routeProvider){
 
